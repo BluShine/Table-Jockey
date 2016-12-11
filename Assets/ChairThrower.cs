b91799d77ffb09feae531aac248ea426f45d6e0d
Mark <umbreon27@gmail.com>
2016-12-10 17:00:01 -0800

    Furniture movement.
    Furniture can now be inhabited and pushed around the room.

--- Assets/ChairThrower.cs
+++ Assets/ChairThrower.cs
@@ -7,54 +7,100 @@ public class ChairThrower : MonoBehaviour {
 
     public GameObject chairPrefab;
 
+    public PhysicMaterial physMaterial;
+
 	// Use this for initialization
 	void Start () {
-		
+		for(int i = 0; i < 10; i++)
+        {
+            GameObject thing;
+            if(Random.value < .5f)
+            {
+                thing = makeChair();
+            } else if (Random.value < .5f)
+            {
+                thing = makeTable();
+            } else
+            {
+                thing = makeBed();
+            }
+
+            throwThing(thing, new Vector3(Random.Range(-4, 4), 1, Random.Range(-4, 4)),
+                new Vector3(.5f - Random.value, .25f - Random.value * .5f, .5f - Random.value), 5);
+        }
 	}
+
+    GameObject makeChair()
+    {
+        GameObject chair = Instantiate(chairPrefab);
+        chair.GetComponent<MeshFilter>().mesh = ChairGenerator.Chair(.1f, .44f, .51f, .56f, .05f, .39f, false, false,
+            Color.HSVToRGB(Random.value, Random.Range(.5f, 1f), 1)).ToMesh();
+        GameObject collider = ChairGenerator.ChairCollider(.1f, .44f, .51f, .56f, .05f, .39f, false, false);
+        collider.transform.parent = chair.transform;
+        foreach(Collider c in chair.GetComponentsInChildren<Collider>())
+        {
+            c.material = physMaterial;
+        }
+        
+        return chair;
+    }
+
+    GameObject makeTable()
+    {
+        GameObject table = Instantiate(chairPrefab);
+        table.GetComponent<MeshFilter>().mesh = TableGenerator.Table(.1f, .6f, 1.2f, .8f, .05f,
+            Color.HSVToRGB(Random.value, Random.Range(.5f, 1f), 1)).ToMesh();
+        GameObject collider = TableGenerator.TableCollider(.1f, .6f, 1.2f, .8f, .05f);
+        collider.transform.parent = table.transform;
+        foreach (Collider c in table.GetComponentsInChildren<Collider>())
+        {
+            c.material = physMaterial;
+        }
+        
+        return table;
+    }
+
+    GameObject makeBed()
+    {
+        GameObject bed = Instantiate(chairPrefab);
+        bed.GetComponent<MeshFilter>().mesh = BedGenerator.Bed(.1f, .3f, 1, 2, .2f, .2f,
+            .07f, .2f, .4f, .65f, .3f,
+            Color.HSVToRGB(Random.value, Random.Range(.5f, 1f), 1),
+            Color.HSVToRGB(Random.value, Random.Range(.5f, 1f), 1)).ToMesh();
+        GameObject collider = BedGenerator.BedCollider(.1f, .3f, 1, 2, .2f, .3f,
+            .07f, .2f, .4f, .65f, .3f);
+        collider.transform.parent = bed.transform;
+        foreach (Collider c in bed.GetComponentsInChildren<Collider>())
+        {
+            c.material = physMaterial;
+        }
+        
+        return bed;
+    }
+
+    void throwThing(GameObject thing, Vector3 pos, Vector3 dir, float force)
+    {
+        thing.transform.position = pos;
+        thing.transform.rotation = Quaternion.Euler(360 * new Vector3(0, Random.value, 0));
+        Rigidbody body = thing.GetComponent<Rigidbody>();
+        body.velocity = dir.normalized * force;
+        //body.angularVelocity = 10 * new Vector3(.5f - Random.value, .5f - Random.value, .5f - Random.value);
+    }
 	
 	// Update is called once per frame
 	void Update () {
+        /*
 		if(Input.GetButtonDown("Fire1"))
         {
-            GameObject chair = Instantiate(chairPrefab);
-            chair.GetComponent<MeshFilter>().mesh = ChairGenerator.Chair(.1f, .44f, .51f, .56f, .05f, .39f, false, false,
-                Color.HSVToRGB(Random.value, Random.Range(.5f, 1f), 1)).ToMesh();
-            GameObject collider = ChairGenerator.ChairCollider(.1f, .44f, .51f, .56f, .05f, .39f, false, false);
-            collider.transform.parent = chair.transform;
-            chair.transform.position = transform.position;
-            chair.transform.rotation = transform.rotation;
-            Rigidbody body = chair.GetComponent<Rigidbody>();
-            body.velocity = transform.forward * 10f;
-            body.angularVelocity = 10 * new Vector3(.5f - Random.value, .5f - Random.value, .5f - Random.value);
+            throwThing(makeChair(), transform.position, transform.forward, 10);
         }
         if(Input.GetButtonDown("Fire2"))
         {
-            GameObject table = Instantiate(chairPrefab);
-            table.GetComponent<MeshFilter>().mesh = TableGenerator.Table(.1f, .6f, 1.2f, .8f, .05f, 
-                Color.HSVToRGB(Random.value, Random.Range(.5f, 1f), 1)).ToMesh();
-            GameObject collider = TableGenerator.TableCollider(.1f, .6f, 1.2f, .8f, .05f);
-            collider.transform.parent = table.transform;
-            table.transform.position = transform.position;
-            table.transform.rotation = transform.rotation;
-            Rigidbody body = table.GetComponent<Rigidbody>();
-            body.velocity = transform.forward * 10f;
-            body.angularVelocity = 10 * new Vector3(.5f - Random.value, .5f - Random.value, .5f - Random.value);
+            throwThing(makeTable(), transform.position, transform.forward, 10);
         }
         if (Input.GetButtonDown("Fire3"))
         {
-            GameObject bed = Instantiate(chairPrefab);
-            bed.GetComponent<MeshFilter>().mesh = BedGenerator.Bed(.1f, .3f, 1, 2, .2f, .2f, 
-                .07f, .2f, .4f, .65f, .3f, 
-                Color.HSVToRGB(Random.value, Random.Range(.5f, 1f), 1), 
-                Color.HSVToRGB(Random.value, Random.Range(.5f, 1f), 1)).ToMesh();
-            GameObject collider = BedGenerator.BedCollider(.1f, .3f, 1, 2, .2f, .3f,
-                .07f, .2f, .4f, .65f, .3f);
-            collider.transform.parent = bed.transform;
-            bed.transform.position = transform.position;
-            bed.transform.rotation = transform.rotation;
-            Rigidbody body = bed.GetComponent<Rigidbody>();
-            body.velocity = transform.forward * 10f;
-            body.angularVelocity = 10 * new Vector3(.5f - Random.value, .5f - Random.value, .5f - Random.value);
-        }
+            throwThing(makeBed(), transform.position, transform.forward, 10);
+        }*/
     }
 }
---
+++ Assets/Player.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float pushForce = 10f;
+
+    Rigidbody inhabitedObject;
+
+    SmoothMouseLook mLook;
+
+	// Use this for initialization
+	void Start () {
+        mLook = GetComponent<SmoothMouseLook>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (inhabitedObject == null)
+        {
+            mLook.enabled = true;
+            if (Input.GetButtonDown("Fire1"))
+            {
+                clickFurniture();
+            }
+        } else
+        {
+            mLook.enabled = false;
+            transform.position = inhabitedObject.transform.position + Vector3.up * 5f;
+            transform.rotation = inhabitedObject.transform.rotation * Quaternion.Euler(90, 0, 0);
+
+            //move
+            Vector3 moveForce = Vector3.zero;
+            moveForce += Input.GetAxis("Vertical") * inhabitedObject.transform.forward;
+            moveForce += Input.GetAxis("Horizontal") * inhabitedObject.transform.right;
+            inhabitedObject.AddForce(moveForce * pushForce, ForceMode.Acceleration);
+            //rotate
+            inhabitedObject.AddTorque(new Vector3(0, Input.GetAxis("Rotate"), 0) * pushForce * 2, ForceMode.Acceleration);
+
+            //keep it from flipping
+            inhabitedObject.transform.rotation = Quaternion.Euler(0, inhabitedObject.transform.rotation.eulerAngles.y, 0);
+
+            if (Input.GetButtonDown("Fire1"))
+            {
+                inhabitedObject = null;
+            }
+        }
+    }
+
+    void clickFurniture()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit rayHit = new RaycastHit();
+        if (Physics.Raycast(ray, out rayHit))
+        {
+            Furniture furnishing = rayHit.collider.transform.GetComponent<Furniture>();
+            if (furnishing == null)
+            {
+                furnishing = rayHit.collider.transform.GetComponentInParent<Furniture>();
+            }
+            if (furnishing != null)
+            {
+                inhabitedObject = furnishing.GetComponent<Rigidbody>();
+                mLook.enabled = false;
+            }
+        }
+    }
+
+    void rightFurniture()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit rayHit = new RaycastHit();
+        if (Physics.Raycast(ray, out rayHit))
+        {
+            Furniture furnishing = rayHit.collider.transform.GetComponent<Furniture>();
+            if (furnishing == null)
+            {
+                furnishing = rayHit.collider.transform.GetComponentInParent<Furniture>();
+            }
+            if (furnishing != null)
+            {
+                furnishing.transform.rotation = Quaternion.Euler(0, 0, 0);
+                furnishing.transform.position.Scale(new Vector3(1, 0, 1));
+            }
+        }
+    }
+
+    void pushFurniture(Vector3 force)
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit rayHit = new RaycastHit();
+        if (Physics.Raycast(ray, out rayHit))
+        {
+            Furniture furnishing = rayHit.collider.transform.GetComponent<Furniture>();
+            if(furnishing == null)
+            {
+                furnishing = rayHit.collider.transform.GetComponentInParent<Furniture>();
+            }
+            if (furnishing != null)
+            {
+                Rigidbody body = furnishing.GetComponent<Rigidbody>();
+                body.AddForceAtPosition(force, rayHit.point);
+            }
+        }
+    }
+}
